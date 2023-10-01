339b0bd218e1e4a42b9218148b320875c61f6ccf
artem2-sl <83166818+artem2-sl@users.noreply.github.com>
2023-10-01 12:24:39 +0300

    Add files via upload

---
+++ KURSOVA/login.js
@@ -0,0 +1,66 @@
+function sign_in_up_google(){
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+var user = result.user;
+
+console.log(user)
+
+
+  }).catch((error) => {
+ 
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    var email = error.email;
+    console.log(errorCode)
+    console.log(errorMessage)
+    console.log(erroremail)
+ 
+  });
+}
+function reg(){
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      data_reg(user.uid)
+      console.log(user)
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage)
+      // ..
+    });
+
+
+   
+}
+
+function data_reg(id){
+    let user = {
+      email: email.value,
+      name: names.value,
+      phone: phone.value,
+      login: login.value
+    }
+    db.collection('users_info').doc(id).set(user).then(res => {
+      alert('Успіх')
+    })
+}
+
+function email_pass_signIn(){
+  firebase.auth().signInWithEmailAndPassword(email_vhid.value, password_vhid.value)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
